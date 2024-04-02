5c609d5b1df6cc352c4816782fde8d9d51f740c5
Tsinswreng <2290019838@qq.com>
2024-04-02 15:00:15 +0800

    更新入口app.ts app.js

--- app.ts
+++ app.ts
@@ -1,11 +1,17 @@
-require('module-alias/register');
-require('tsconfig-paths/register');
+//@ts-check
+// require('module-alias/register');
+// require('tsconfig-paths/register');
+// Error.stackTraceLimit = 99
+// // worked
+// //const express = require('express')
+// //console.log(express)
+// //const sqlite3 = require('sqlite3')
+// //console.log(sqlite3)
+// const VocaServer = require(`./src/backend/VocaServer`)
+// VocaServer.default.main()
+// //console.log(VocaServer)
+
 Error.stackTraceLimit = 99
-// worked
-//const express = require('express')
-//console.log(express)
-//const sqlite3 = require('sqlite3')
-//console.log(sqlite3)
-const VocaServer = require(`./src/backend/VocaServer`)
-VocaServer.default.main()
-//console.log(VocaServer)
+//import VocaServer from './out/backend/VocaServer'
+import VocaServer from "./src/backend/VocaServer"
+VocaServer.main()
