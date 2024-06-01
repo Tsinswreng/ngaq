8ba94b0bbc410017417a8a62d51e66b0aab8ef61
Tsinswreng <2290019838@qq.com>
2024-06-02 00:33:57 +0800

    tsv handleLine獨立; 支持 no comment

--- src/backend/rime/tsv.ts
+++ src/backend/rime/tsv.ts
@@ -7,13 +7,20 @@ interface I_next<T>{
 	next():T
 }
 
+export interface I_readN<T>{
+	read(n:int):T
+}
+
 
 class Status{
-	noComment=false
+	nextLineNoComment=false
 	in_meta = false
 	in_body = false
 }
 
+class LineType{
+	noComment=false
+}
 
 class Line{
 	protected static lineCommentMark = '#'
@@ -30,6 +37,7 @@ class Line{
 	}
 	text:str
 	index:int
+	type = new LineType()
 
 	/**
 	 * 取字串芝除註釋後者
@@ -51,16 +59,23 @@ export class TsvParser{
 
 	protected constructor(){}
 
-	protected __init__(){
-
+	protected __init__(...args:Parameters<typeof TsvParser.new>){
+		const z = this
+		z._readNObj = args[0]
+		return z
 	}
 
-	static new(){
-
+	static new(readNObj:I_readN<Promise<str[]>>){
+		const z = new this()
+		z.__init__(readNObj)
+		return z
 	}
 
-	protected _iter:I_next<Promise<str>>
-	get iter(){return this._iter}
+	// protected _nextObj:I_next<Promise<str>>
+	// get nextObj(){return this._nextObj}
+
+	protected _readNObj:I_readN<Promise<str[]>>
+	get readNObj(){return this._readNObj}
 
 	protected _status = new Status()
 	get status(){return this._status}
@@ -116,12 +131,30 @@ export class TsvParser{
 		return endPos
 	}
 
+	async readLines(num:int){
+		const z = this
+		const lines = await z.readNObj.read(num)
+		const ans:Line[] = []
+		for(const lineTxt of lines){
+			const ua = z.handleLine(lineTxt)
+			ans.push(ua)
+		}
+		return ans
+	}
 
-	async nextLine(){
+	handleLine(lineTxt:str){
 		const z = this
 		z._linePos ++
-		const lineTxt = await z._iter.next()
+		// const lineTxt = await z._nextObj.next()
+		const noCommentPattern = '# no comment'
 		const line = Line.new(lineTxt, z._linePos)
+
+		if(z.status.nextLineNoComment === true){ //即上一行有 no comment 指令
+			line.type.noComment = true
+		}else{
+			line.type.noComment = false
+		}
+
 		if(z.status.in_body === false){
 			if(lineTxt === '---'){
 				z.status.in_meta = true
@@ -132,6 +165,11 @@ export class TsvParser{
 				z.status.in_meta = false
 				z.status.in_body = true
 				z._metaEndLinePos = z.linePos
+			}
+		}else{ //在body
+			z._status.nextLineNoComment = false //褈置
+			if(lineTxt === noCommentPattern){
+				z._status.nextLineNoComment = true
 			}
 		}
 		return line
--- test/backend/rime/tsv.test.ts
+++ test/backend/rime/tsv.test.ts
@@ -1,3 +1,28 @@
 import * as Tsv from '@backend/rime/tsv'
+import * as File from '@backend/util/File'
+
+
+describe('read', ()=>{
+
+	it('1', async()=>{
+		const path = process.cwd()+'/test/backend/rime/dict.dict.yaml'
+		const frl = File.FileReadLine.new(path, {encoding:'utf-8'})
+		const readN:Tsv.I_readN<Promise<string[]>> = {
+			read(n:number){
+				return frl.read(n)
+			}
+		}
+		const tsvParser = Tsv.TsvParser.new(readN)
+		for(let i = 0; i < 100; i++){
+			const line = await tsvParser.readLines(1)
+			//console.log(line[0].text)
+			console.log()
+		}
+		expect(tsvParser.metaStartLinePos).toBe(18)
+		expect(tsvParser.metaEndLinePos).toBe(41)
+	})
+	
+})
+
 
 
