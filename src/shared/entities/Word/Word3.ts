da339e3a06454b164ca8798674e044b43939e34e
Tsinswreng <2290019838@qq.com>
2024-06-22 10:00:29 +0800

    褈構Word3.ts
    分靜態類與實例類、便于繼承與類型約束

--- src/shared/entities/Word/Word3.ts
+++ src/shared/entities/Word/Word3.ts
@@ -5,6 +5,7 @@ import { NonFuncProp, SetterProp } from '@shared/Type'
 import { As } from '@shared/Ut';
 
 
+
 export function assignExisting(a:kvobj,b:kvobj){
 	for(let key in a){
 		if(b.hasOwnProperty(key)){
@@ -22,19 +23,19 @@ function assign(a:kvobj, b:kvobj){
 type id_t = int
 
 export interface I_id{
-	id:id_t
+	[Rows.Row.col.id]:id_t
 }
 
 export interface I_ct{
-	ct:Tempus
+	[Rows.Row.col.ct]:Tempus
 }
 
 export interface I_mt{
-	mt:Tempus
+	[Rows.Row.col.mt]:Tempus
 }
 
 export interface I_belong{
-	belong:str
+	[Rows.Row.col.belong]:str
 }
 
 export interface I_ctMt extends I_ct, I_mt{
@@ -85,249 +86,348 @@ export class IdCtMt{
 
 }
 
+interface I_BaseInst extends I_idBelongCtMt{
+	__init__():I_BaseInst
+	This
+}
 
-class Base<RowType extends Rows.Row = Rows.Row> extends IdCtMt{
-	protected constructor(){super()}
-	protected __init__(...args: Parameters<typeof Base.new>){
-		const z = this
-		return z
+interface I_BaseStatic<RowT extends Rows.Row>{
+	new :(...args:any[])=>I_BaseInst
+	correctObj(obj:I_BaseInst):I_BaseInst
+	correctRow(row:RowT):RowT
+	fromRow(row:RowT):I_BaseInst
+	Row:typeof Rows.Row
+}
+
+class BaseInst implements I_BaseInst{
+	__init__(){
+		return this
 	}
+	This=BaseInst;
+	;[Rows.Row.col.id]: number;
+	get id_(){return this.id}
+	protected set id_(v){this.id = v}
+	
+	[Rows.Row.col.ct]: Tempus;
+	get ct_(){return this.ct}
+	protected set ct_(v){this.ct = v}
 
-	static new(...args:any[]):IdCtMt
-	static new(){
-		//@ts-ignore
-		const z = new this()
-		z.__init__()
-		return z
-	}
+	[Rows.Row.col.mt]: Tempus;
+	get mt_(){return this.mt}
+	protected set mt_(v){this.mt = v}
 
-	//@ts-ignore
-	get This(){return Base}
-
-	protected [Rows.Row.col.belong]:str
+	[Rows.Row.col.belong]: string;
 	get belong_(){return this.belong}
 	protected set belong_(v){this.belong = v}
+}
 
-	static correctObj(w:Base){
-		w.ct = Tempus.new(As(w.ct, 'number'))
-		w.ct = Tempus.new(As(w.mt, 'number'))
-		return w
+class BaseStatic<InstType extends BaseInst> implements I_BaseStatic<Rows.Row>{
+	new(...args:any[]){
+		const z = new BaseInst()
+		z.__init__()
+		return z as InstType
 	}
-
-
-	correctObj(w:Parameters<typeof Base.correctObj>[0]):this
-	correctObj(w:Parameters<typeof Base.correctObj>[0]){
-		const z = this
-		return z.This.correctObj(w)
+	Row=Rows.Row
+	Inst=BaseInst
+	correctObj(obj: I_BaseInst): I_BaseInst {
+		obj.ct = Tempus.new(As(obj.ct, 'number'))
+		obj.ct = Tempus.new(As(obj.mt, 'number'))
+		return obj
 	}
-
-	static correctRow(row:Rows.Row){
+	correctRow(row: Rows.Row): Rows.Row {
 		row.ct = Tempus.toUnixTime_mills(As(row.ct, Tempus))
 		row.mt = Tempus.toUnixTime_mills(As(row.mt, Tempus))
 		return row
 	}
-
-	correctRow(row:Parameters<typeof Base.correctRow>[0]):RowType
-	correctRow(row:Parameters<typeof Base.correctRow>[0]){
+	fromRow(row: Rows.Row): I_BaseInst {
 		const z = this
-		return z.This.correctRow(row)
-	}
-
-}
-
-class Pa{
-	fn(prop:{foo:str}){
-
-	}
-}
-
-class Ch extends Pa{
-	override fn(prop:{foo:str, bar:number}){
-
-	}
-}
-
-
-export class Word extends Base<Rows.WordRow>{
-	protected constructor(){super()}
-	protected __init__(...args: Parameters<typeof Word.new>){
-		const z = this
-		const prop = args[0]
-		Object.assign(z, prop)
-		return z
-	}
-
-	static new(...args:any[]):never
-	static new(prop:{
-		id?:int
-		,belong:str
-		,text:str
-		,ct:Tempus
-		,mt:Tempus
-	}){
-		const z = new this()
-		z.__init__(prop)
-		return z
-	}
-
-
-	//@ts-ignore
-	get This(){return Word}
-
-	// protected [Rows.WordRow.col.id]?:int
-	// get id_(){return this.id}
-	// set id_(v){this.id = v}
-	
-	// protected [Rows.WordRow.col.belong]:str
-	// get belong_(){return this.belong}
-	// set belong_(v){this.belong = v}
-
-	protected [Rows.WordRow.col.text]:str
-	get text_(){return this.text}
-	protected set text_(v){this.text = v}
-
-	static correctObj(w:Word):Word
-	static correctObj(word:Word){
-		return super.correctObj(word as Base) as Word
-		//return super.correctObj(As(word, Base)) as Word
-	}
-
-	static correctRow(row:Rows.WordRow):Rows.WordRow
-	static correctRow(row:Rows.WordRow){
-		// row.ct = Tempus.toUnixTime_mills(As(row.ct, Tempus))
-		// row.mt = Tempus.toUnixTime_mills(As(row.mt, Tempus))
-		return super.correctRow(row)
-		//return row
-	}
-
-	static fromRow(r:Rows.WordRow){
-		const c = Rows.WordRow.col
-		const ans = new Word()
-		assign(ans,r)
-		Word.correctObj(ans)
-		return ans
-		// const word = Word.new({
-		// 	[c.id] : r.id
-		// 	,[c.belong]:r.belong
-		// 	,[c.text]:r.text
-		// 	,[c.ct]:Tempus.new(r.ct)
-		// 	,[c.mt]:Tempus.new(r.mt)
-		// })
-		// return word
-	}
-
-	fromRow(...args:Parameters<typeof Word.fromRow>){
-		return Word.fromRow(...args)
-	}
-
-	static toRow(w:Word){
-		const c = Rows.WordRow.col
-		const ans = new Rows.WordRow
-		assign(ans, w)
-		Word.correctRow(ans)
-		return ans
-		// const r:Rows.WordRow={
-		// 	[c.id]: w.id
-		// 	,[c.belong]: w.belong
-		// 	,[c.text]: w.text
-		// 	,[c.ct]: Tempus.toUnixTime_mills(w.ct)
-		// 	,[c.mt]: Tempus.toUnixTime_mills(w.mt)
-		// }
-		// return r
-	}
-
-	toRow(){
-		return Word.toRow(this)
-	}
-}
-
-export class Learn extends Base<Rows.LearnRow>{
-	protected constructor(){super()}
-	protected __init__(...args: Parameters<typeof Learn.new>){
-		const z = this
-		return z
-	}
-
-	static new(...args:any[]):never
-	static new(){
-		const z = new this()
-		z.__init__()
-		return z
-	}
-
-	//@ts-ignore
-	get This(){return Learn}
-
-	static correctRow(row:Rows.LearnRow){
-		return super.correctRow(row)
-	}
-
-	//static correctObj(obj:Learn):Learn
-	static correctObj(obj:Learn){
-		return super.correctObj(obj as Base) as Learn
-	}
-
-	static fromRow(row:Rows.LearnRow){
-		const ans = new this()
+		const ans = new z.Inst()
 		assign(ans, row)
-		return this.correctObj(ans)
+		z.correctObj(ans)
+		return ans
 	}
+}
 
-	static toRow(z:Learn){
-		const row = new Rows.LearnRow()
-		assign(row, z)
-		return this.correctRow(row)
-	}
+class WordInst extends BaseInst{
+
+}
+
+class WordStatic extends BaseStatic<WordInst>{
+
+}
+export const Word = new WordStatic()
+
+class PropertyInst extends BaseInst{
+
+}
+
+class PropertyStatic extends BaseStatic<PropertyInst>{
+
+}
+
+export const Property = new PropertyStatic()
+
+class LearnInst extends BaseInst{
+
+}
+
+class LearnStatic extends BaseStatic<LearnInst>{
+
+}
+
+export const Learn = new LearnStatic()
+
+
+
+
+
+
+
+// class Base_0<RowType extends Rows.Row = Rows.Row> extends IdCtMt{
+// 	protected constructor(){super()}
+// 	protected __init__(...args: Parameters<typeof Base_0.new>){
+// 		const z = this
+// 		return z
+// 	}
+
+// 	static new(...args:any[]):IdCtMt
+// 	static new(){
+// 		//@ts-ignore
+// 		const z = new this()
+// 		z.__init__()
+// 		return z
+// 	}
+
+// 	//@ts-ignore
+// 	get This(){return Base_0}
+
+// 	protected [Rows.Row.col.belong]:str
+// 	get belong_(){return this.belong}
+// 	protected set belong_(v){this.belong = v}
+
+// 	static correctObj(w:Base_0){
+// 		w.ct = Tempus.new(As(w.ct, 'number'))
+// 		w.ct = Tempus.new(As(w.mt, 'number'))
+// 		return w
+// 	}
+
+
+// 	correctObj(w:Parameters<typeof Base_0.correctObj>[0]):this
+// 	correctObj(w:Parameters<typeof Base_0.correctObj>[0]){
+// 		const z = this
+// 		return z.This.correctObj(w)
+// 	}
+
+// 	static correctRow(row:Rows.Row){
+// 		row.ct = Tempus.toUnixTime_mills(As(row.ct, Tempus))
+// 		row.mt = Tempus.toUnixTime_mills(As(row.mt, Tempus))
+// 		return row
+// 	}
+
+// 	correctRow(row:Parameters<typeof Base_0.correctRow>[0]):RowType
+// 	correctRow(row:Parameters<typeof Base_0.correctRow>[0]){
+// 		const z = this
+// 		return z.This.correctRow(row)
+// 	}
+
+// }
+
+// class Pa{
+// 	fn(prop:{foo:str}){
+
+// 	}
+// }
+
+// class Ch extends Pa{
+// 	override fn(prop:{foo:str, bar:number}){
+
+// 	}
+// }
+
+
+// export class Word_0 extends Base_0<Rows.WordRow>{
+// 	protected constructor(){super()}
+// 	protected __init__(...args: Parameters<typeof Word_0.new>){
+// 		const z = this
+// 		const prop = args[0]
+// 		Object.assign(z, prop)
+// 		return z
+// 	}
+
+// 	static new(...args:any[]):never
+// 	static new(prop:{
+// 		id?:int
+// 		,belong:str
+// 		,text:str
+// 		,ct:Tempus
+// 		,mt:Tempus
+// 	}){
+// 		const z = new this()
+// 		z.__init__(prop)
+// 		return z
+// 	}
+
+
+// 	//@ts-ignore
+// 	get This(){return Word_0}
+
+// 	// protected [Rows.WordRow.col.id]?:int
+// 	// get id_(){return this.id}
+// 	// set id_(v){this.id = v}
 	
-	toRow(z=this){
-		return z.This.toRow(z)
-	}
+// 	// protected [Rows.WordRow.col.belong]:str
+// 	// get belong_(){return this.belong}
+// 	// set belong_(v){this.belong = v}
 
-}
+// 	protected [Rows.WordRow.col.text]:str
+// 	get text_(){return this.text}
+// 	protected set text_(v){this.text = v}
 
-export class Property extends Base<Rows.PropertyRow>{
-	protected constructor(){super()}
-	protected __init__(...args: Parameters<typeof Property.new>){
-		const z = this
-		const prop = args[0]
-		Object.assign(z, prop)
-		return z
-	}
+// 	static correctObj(w:Word_0):Word_0
+// 	static correctObj(word:Word_0){
+// 		return super.correctObj(word as Base_0) as Word_0
+// 		//return super.correctObj(As(word, Base)) as Word
+// 	}
 
-	static new(prop:{
-		id?:int
-		,belong:Rows.PropertyBelong
-		,wid?:int
-	}){
-		const z = new this()
-		z.__init__(prop)
-		return z
-	}
+// 	static correctRow(row:Rows.WordRow):Rows.WordRow
+// 	static correctRow(row:Rows.WordRow){
+// 		// row.ct = Tempus.toUnixTime_mills(As(row.ct, Tempus))
+// 		// row.mt = Tempus.toUnixTime_mills(As(row.mt, Tempus))
+// 		return super.correctRow(row)
+// 		//return row
+// 	}
+
+// 	static fromRow(r:Rows.WordRow){
+// 		const c = Rows.WordRow.col
+// 		const ans = new Word_0()
+// 		assign(ans,r)
+// 		Word_0.correctObj(ans)
+// 		return ans
+// 		// const word = Word.new({
+// 		// 	[c.id] : r.id
+// 		// 	,[c.belong]:r.belong
+// 		// 	,[c.text]:r.text
+// 		// 	,[c.ct]:Tempus.new(r.ct)
+// 		// 	,[c.mt]:Tempus.new(r.mt)
+// 		// })
+// 		// return word
+// 	}
+
+// 	fromRow(...args:Parameters<typeof Word_0.fromRow>){
+// 		return Word_0.fromRow(...args)
+// 	}
+
+// 	static toRow(w:Word_0){
+// 		const c = Rows.WordRow.col
+// 		const ans = new Rows.WordRow
+// 		assign(ans, w)
+// 		Word_0.correctRow(ans)
+// 		return ans
+// 		// const r:Rows.WordRow={
+// 		// 	[c.id]: w.id
+// 		// 	,[c.belong]: w.belong
+// 		// 	,[c.text]: w.text
+// 		// 	,[c.ct]: Tempus.toUnixTime_mills(w.ct)
+// 		// 	,[c.mt]: Tempus.toUnixTime_mills(w.mt)
+// 		// }
+// 		// return r
+// 	}
+
+// 	toRow(){
+// 		return Word_0.toRow(this)
+// 	}
+// }
+
+// export class Learn_0 extends Base_0<Rows.LearnRow>{
+// 	protected constructor(){super()}
+// 	protected __init__(...args: Parameters<typeof Learn_0.new>){
+// 		const z = this
+// 		return z
+// 	}
+
+// 	static new(...args:any[]):never
+// 	static new(){
+// 		const z = new this()
+// 		z.__init__()
+// 		return z
+// 	}
+
+// 	//@ts-ignore
+// 	get This(){return Learn_0}
+
+// 	static correctRow(row:Rows.LearnRow){
+// 		return super.correctRow(row)
+// 	}
+
+// 	//static correctObj(obj:Learn):Learn
+// 	static correctObj(obj:Learn_0){
+// 		return super.correctObj(obj as Base_0) as Learn_0
+// 	}
+
+// 	static fromRow(row:Rows.LearnRow){
+// 		const ans = new this()
+// 		assign(ans, row)
+// 		return this.correctObj(ans)
+// 	}
+
+// 	static toRow(z:Learn_0){
+// 		const row = new Rows.LearnRow()
+// 		assign(row, z)
+// 		return this.correctRow(row)
+// 	}
+	
+// 	toRow(z=this){
+// 		return z.This.toRow(z)
+// 	}
+
+// }
+
+// export class Property_0 extends Base_0<Rows.PropertyRow>{
+// 	protected constructor(){super()}
+// 	protected __init__(...args: Parameters<typeof Property_0.new>){
+// 		const z = this
+// 		const prop = args[0]
+// 		Object.assign(z, prop)
+// 		return z
+// 	}
+
+// 	static new(prop:{
+// 		id?:int
+// 		,belong:Rows.PropertyBelong
+// 		,wid?:int
+// 	}){
+// 		const z = new this()
+// 		z.__init__(prop)
+// 		return z
+// 	}
 
 
-	//@ts-ignore
-	get This(){return Property}
+// 	//@ts-ignore
+// 	get This(){return Property_0}
 
-	static correctRow(row: Rows.Row): Rows.PropertyRow
-	static correctRow(row: Rows.Row){
-		return super.correctRow(row)
-	}
+// 	static correctRow(row: Rows.Row): Rows.PropertyRow
+// 	static correctRow(row: Rows.Row){
+// 		return super.correctRow(row)
+// 	}
 
-	static correctObj(w: Base):Property
-	static correctObj(w: Base){
-		return super.correctObj(w as Base) as Property
-	}
+// 	static correctObj(w: Base_0):Property_0
+// 	static correctObj(w: Base_0){
+// 		return super.correctObj(w as Base_0) as Property_0
+// 	}
 
-	static toRow(z:Property){
-		const row = new Rows.PropertyRow()
-		assign(row, z)
-		return this.correctRow(row)
-	}
+// 	static toRow(z:Property_0){
+// 		const row = new Rows.PropertyRow()
+// 		assign(row, z)
+// 		return this.correctRow(row)
+// 	}
 
-	static fromRow(){
-		
-	}
-}
+// 	static fromRow(){
+
+// 	}
+// }
+
+
+
 
 
 
